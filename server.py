dafd5af0deb1a925efffb0cd1feaddbbd22bd594
unknown <pedroinaciolimao>
2025-01-31 09:44:47 -0300

    atualizando

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import *
 import dao
 app = Flask(__name__)
+
+app.secret_key = 'Khggj3h424j23hg44$#'
+
+@app.route('/logout', methods=['POST', 'GET'])
+def sair():
+    session.pop('login')
+    return render_template('index.html')
 
 @app.route("/")
 def index():
@@ -31,6 +38,7 @@ def login():
         senha = request.form.get("senha")
 
         if len(dao.login(login, senha)) > 0:
+            session['login'] = login
             return render_template("bem_vindo.html")
         else:
             return render_template('login.html')
@@ -40,15 +48,53 @@ def login():
 def home():
     return render_template('bem_vindo.html')
 
+@app.route('/exercicio')
+def exercicio():
+    return render_template('exercicio.html')
+
 @app.route('/treino_peito')
 def treino_peito():
     return render_template('treino_peito.html')
 
 @app.route('/listausuarios')
 def listausuarios():
-    usuarios = dao.listausuarios()
-    print(usuarios)
-    return render_template('Listausuarios.html', lista=usuarios)
+
+    if 'login' in session:
+        usuarios = dao.listausuarios()
+        return render_template('Listausuarios.html', lista=usuarios)
+    else:
+        return render_template('index.html')
+
+@app.route('/listar_exercicios')
+def listar_exercicios():
+    return render_template('listar_exercicios.html', treino=treino)
+# Rota para adicionar um novo exercício
+
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        categoria = request.form['categoria']
+        descricao = request.form['descricao']
+
+        # Adiciona o exercício na lista
+        treino.append({
+            'nome': nome,
+            'categoria': categoria,
+            'descricao': descricao
+        })
+        return redirect(url_for('listar_exercicios'))
+
+    return render_template('cadastrar.html')
+
+
+# Rota para excluir um exercício do treino
+@app.route('/excluir_exercicio', methods=['POST'])
+def excluir_exercicio(exercicio):
+    if 0 <= exercicio < len(treino):
+        treino.pop(exercicio)
+    return redirect(url_for('listar_exercicios'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
